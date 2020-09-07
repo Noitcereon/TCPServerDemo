83931695a69f39e3ba6942c5d50dd9204dc20baa
Thomas (Noit) <tba@live.dk>
2020-09-07 12:33:22 +0200

    Added base server code for ServerDateTime

---
+++ TCPServerLib/Servers/ServerDateTime.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TCPLib.Servers
+{
+    public class ServerDateTime : IServerWorker
+    {
+        public void Start()
+        {
+            TcpListener server = new TcpListener(IPAddress.Loopback, 3003);
+            server.Start();
+
+            while (true)
+            {
+                TcpClient socket = server.AcceptTcpClient();
+                Task.Run(() => HandleClient(socket));
+            }
+        }
+
+        private void HandleClient(TcpClient socket)
+        {
+            NetworkStream ns = socket.GetStream();
+            StreamReader sr = new StreamReader(ns);
+            StreamWriter sw = new StreamWriter(ns);
+
+            string clientMsg = sr.ReadLine();
+
+            sw.WriteLine("output");
+            sw.Flush();
+
+            socket.Close();
+        }
+    }
+}
